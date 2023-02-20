4cf45d392c80ce8d2bac1f0b5eaee235f57245fa
Menno van Voorst <menno.vanvoorst@kpn.com>
2023-02-20 10:49:48 +0100

    fix: minor styling fixes

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { NavList } from '../NavList';
 import type { NavLink } from '../NavLink';
+import Link from 'next/link';
 
 type Navbar = {
   logo: SbAssetData;
@@ -15,13 +16,15 @@ export const Navbar = ({ blok }: Props) => (
   <header className="w-full">
     <div className="h-2 bg-gradient-blue-green"></div>
     <div className="h-16 py-2 flex justify-between items-center container mx-auto">
-      <Image
-        src={blok.logo.filename}
-        alt="Peerpep logo"
-        width={0}
-        height={0}
-        className="w-auto h-full"
-      />
+      <Link href="/" className="h-full">
+        <Image
+          src={blok.logo.filename}
+          alt="Peerpep logo"
+          width={0}
+          height={0}
+          className="w-auto h-full"
+        />
+      </Link>
       <div>
         <NavList items={blok.navigationItems} />
       </div>
